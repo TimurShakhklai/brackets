fc250a074708be4f3c2b6648d1bad0475dfe2b79
TimurShakhlai <70841435+TimurShakhklai@users.noreply.github.com>
2020-10-25 23:41:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let Arr = bracketsConfig.map((i) => i[0] + i[1]);
+    let Str = str;
+    while (true) {
+        for (let i = 0; i < Arr.length; i++) {
+            str = str.replace(Arr[i], '');
+        }
+        if (str.length == 0) {
+            return true;
+        }
+        if (Str === str) {
+            return false;
+        }
+        Str = str;
+    }
 }
